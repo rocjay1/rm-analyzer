62d96c86331b08d60c0de875849db6ff74c028de
Rocco Davino <jasonroc19@gmail.com>
2026-01-25 11:16:26 -0600

    refactor: reformat email imports in `controllers.py`

--- src/backend/rmanalyzer/controllers.py
+++ src/backend/rmanalyzer/controllers.py
@@ -11,7 +11,8 @@ from http import HTTPStatus
 
 import azure.functions as func
 from rmanalyzer import db, storage
-from rmanalyzer.email import send_email, send_error_email, render_body, render_subject
+from rmanalyzer.email import (render_body, render_subject, send_email,
+                              send_error_email)
 from rmanalyzer.models import Group, Person, get_transactions
 
 __all__ = [
